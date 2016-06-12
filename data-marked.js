3456df040eb99ff26a1a75d3f76a37fb05b2f375
Andrew McWatters <mcwattersandrew@gmail.com>
2016-06-12 12:40:52 -0700

    Initial commit

---
+++ data-marked.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  function marked(el) {
+    el.innerHTML = window.marked(el.dataset.marked || el.innerHTML);
+  }
+
+  function markedAll() {
+    var elements = document.querySelectorAll('[data-marked]');
+    for (var i = 0; i < elements.length; i++) {
+      marked(elements[i]);
+    }
+  }
+
+  function ready(fn) {
+    if (document.readyState != 'loading'){
+      fn();
+    } else {
+      document.addEventListener('DOMContentLoaded', fn);
+    }
+  }
+
+  ready(markedAll);
+
+  function update(node) {
+    if (node.dataset && node.dataset.marked) {
+      marked(node);
+      return;
+    }
+
+    var childNodes = node.childNodes;
+    for (var i = 0; i < childNodes.length; i++) {
+      update(childNodes[i]);
+    }
+  }
+
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      mutation.addedNodes.forEach(update);
+    });
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
